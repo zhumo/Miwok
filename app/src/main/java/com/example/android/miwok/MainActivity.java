37ce76b93edb7f9571acb584338bb82ff5b3e17d
mozhu <mozhu888@gmail.com>
2017-07-10 12:48:48 -0400

    main activity links to sub activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent numbersListIntent = new Intent(this, NumbersActivity.class);
+        startActivity(numbersListIntent);
+    }
+
+    public void openFamilyMembersList(View view) {
+        Intent familyMembersListIntent = new Intent(this, FamilyMembersActivity.class);
+        startActivity(familyMembersListIntent);
+    }
+
+    public void openColorsList(View view) {
+        Intent colorsListIntent = new Intent(this, ColorsActivity.class);
+        startActivity(colorsListIntent);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent categoriesListIntent = new Intent(this, PhrasesActivity.class);
+        startActivity(categoriesListIntent);
     }
 }
