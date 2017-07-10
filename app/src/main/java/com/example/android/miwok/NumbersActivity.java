d4dd783740842eaa5acc534421f4d22d86895a2f
mozhu <mozhu888@gmail.com>
2017-07-10 16:20:36 -0400

    loop through numbers and render

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbers = new ArrayList<String>();
+        numbers.add("one");
+        numbers.add("two");
+        numbers.add("three");
+        numbers.add("four");
+        numbers.add("five");
+        numbers.add("six");
+        numbers.add("seven");
+        numbers.add("eight");
+        numbers.add("nine");
+        numbers.add("ten");
+
+        LinearLayout numbersList = (LinearLayout) findViewById(R.id.numbers_list);
+        for (String number : numbers) {
+            TextView numberView = new TextView(this);
+            numberView.setText(number);
+            numbersList.addView(numberView);
+        }
     }
 }
